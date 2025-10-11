2e27f603ec379b50daded369d665446488314d58
muratguven <murat.guven@vodafone.com>
2025-10-11 18:26:15 +0300

    seperated rate limits for auth and other paths

--- src/middleware/rateLimitMiddleware.ts
+++ src/middleware/rateLimitMiddleware.ts
@@ -1,8 +1,18 @@
 import rateLimit from 'express-rate-limit';
 
-export const apiRateLimiter = rateLimit({
+export const authApiRateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 dakika
   max: 10, // Her IP için maksimum 10 istek/dk
+  standardHeaders: true, // RateLimit-* headers gönder
+  legacyHeaders: false,  // X-RateLimit-* header'larını gönderme
+  message: {
+    error: 'Too many login or register attempts, please try again later.',
+  },
+});
+
+export const apiRateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 dakika
+  max: 100, // Her IP için maksimum 100 istek/dk
   standardHeaders: true, // RateLimit-* headers gönder
   legacyHeaders: false,  // X-RateLimit-* header'larını gönderme
   message: {
